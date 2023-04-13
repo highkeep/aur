62faa6a9b719ca2ba2ef5560776fa3b3269154fd
Nick Marshall <nmarshall@highkeep.com>
2023-04-13 12:16:01 -0700

    workflow version testing

--- versions.sh
+++ versions.sh
@@ -5,4 +5,4 @@ PKG=${1}
 remoteVersion=$(git ls-remote -q https://aur.archlinux.org/${PKG}.git heads/master | cut -f1)
 currentVersion=$(cat versions/${PKG} 2>/dev/null)
 
-[ ${remoteVersion} != ${currentVersion} ] && echo update=true || echo update=false
+[[ ${remoteVersion} != ${currentVersion} ]] && echo update=true || echo update=false
