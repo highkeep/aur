74384c41aacaef2fd9b993da9c46b9eb4b064652
Nick Marshall <nmarshall@highkeep.com>
2023-04-13 13:46:32 -0700

    rerun afdko

--- functions.sh
+++ functions.sh
@@ -6,7 +6,7 @@ checkVersion() {
     remoteVersion=$(git ls-remote -q https://aur.archlinux.org/${PKG}.git heads/master | cut -f1)
     currentVersion=$(cat versions/${PKG} 2>/dev/null)
 
-    if [ "${remoteVersion}" != "${currentVersion}" ]; then
+    if [[ "${remoteVersion}" != "${currentVersion}" ]]; then
         echo update=true
     else
         echo update=false
